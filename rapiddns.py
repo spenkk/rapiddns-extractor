3c4a57c40b1cad1dbf69a62faaf5022ebce76932
Arben Shala <43441596+spenkk@users.noreply.github.com>
2022-11-24 19:44:42 +0100

    Update rapiddns.py
    Changed the code to show only subdomains and not other data

--- rapiddns.py
+++ rapiddns.py
@@ -1,52 +1,37 @@
 import os, requests, sys, csv, colorama
 from colorama import Fore, Style
 from bs4 import BeautifulSoup
+requests.packages.urllib3.disable_warnings()
 
 if len(sys.argv)<2:
 	print("Usage: python " + sys.argv[0] + " target.com")
 	sys.exit(1)
 
-def onlysubdomains():
-	with open("all.txt") as lines:
-		for i in lines:
-			url = i.split(",")[0]
-			domain = url.split("//")[-1].split("/")[0]
-			
-			with open ('domains-temp.txt', 'a') as temp:
-					temp.write(domain + '\n')
-
-		# Sorting and removing duplicates
-		lines_seen = set()
-		outfile = open('subdomains.txt', 'w')
-		for line in open('domains-temp.txt', 'r'):
-			if line not in lines_seen:
-				if not line.strip(): continue
-				outfile.write(line)
-				lines_seen.add(line)
-				print(Fore.GREEN + '' + line.rstrip() + '' + Style.RESET_ALL)
-		outfile.close()
-		os.remove('domains-temp.txt')
-
-def withDescription():
-	for website_name in website_table_items:
-		web = website_name.contents[-1].strip()
-		urls = website_name['href'] + ', "' + web + '"' 
-		with open('all.txt', 'a') as out:
-			out.write(urls + '\n')
-
-
 
 url = "https://rapiddns.io/subdomain/{}?full=1#result".format(sys.argv[1])
 
-page = requests.get(url)
+page = requests.get(url, verify=False)
 soup = BeautifulSoup(page.text, 'html.parser')
 
 output_rows = []
 website_table = soup.find("table",{"class":"table table-striped table-bordered"})
-website_table_items = website_table.find_all('a', href=True)
+website_table_items = website_table.find_all('tbody')
+for tbody in website_table_items:
+	tr = tbody.find_all('tr')
+	for td in tr:
+		subdomain = td.find_all('td')[0].text
 
+		# Sorting and removing duplicates			
+		with open ('domains-temp.txt', 'a') as temp:
+			temp.write(subdomain + '\n')
 
-if __name__ == "__main__":
-	
-	withDescription()
-	onlysubdomains()
+	lines_seen = set()
+	outfile = open('{}-rapiddns.out'.format(sys.argv[1]), 'w')
+	for line in open('domains-temp.txt', 'r'):
+		if line not in lines_seen:
+			if not line.strip(): continue
+			outfile.write(line)
+			lines_seen.add(line)
+			print(Fore.GREEN + '' + line.rstrip() + '' + Style.RESET_ALL)
+	outfile.close()
+	os.remove('domains-temp.txt')
